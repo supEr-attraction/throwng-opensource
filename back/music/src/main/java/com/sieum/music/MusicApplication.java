de370b3ec577194d69db74b40e3b153fc9889927
임승연 <lsy5490@naver.com>
2024-04-23 00:41:50 +0900

    Revert "Merge branch 'feat/#7' into 'master'"

--- back/music/src/main/java/com/sieum/music/MusicApplication.java
+++ back/music/src/main/java/com/sieum/music/MusicApplication.java
@@ -2,10 +2,8 @@ package com.sieum.music;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class MusicApplication {
 
     public static void main(String[] args) {
