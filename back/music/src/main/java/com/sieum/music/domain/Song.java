88c12d432725a4e0f11bd20322451e841b35c535
김민준 <yid0816@naver.com>
2024-04-23 21:53:37 +0900

    [Feature] Create Search Menu, Drop Music

---
+++ back/music/src/main/java/com/sieum/music/domain/Song.java
@@ -0,0 +1,34 @@
+package com.sieum.music.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Song {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "song_id")
+    private Integer id;
+
+    @Column(length = 400)
+    @NotNull
+    private String title;
+
+    @Column(length = 200)
+    @NotNull
+    private String albumImage;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "artist_id")
+    @NotNull
+    private Artist artist;
+}
---
+++ front/src/components/musicSearch/MusicList.tsx
@@ -0,0 +1,42 @@
+import { useNavigate } from "react-router-dom"
+import {Song} from "../../types/songType.ts"
+import SearchedWords from "./SearchedWords.tsx"
+import "@styles/musicSearch/MusicList.scss"
+
+interface Props {
+  searchResults?: Song[],
+  onWordClick: (title: string) => void,
+}
+
+const MusicList = ({searchResults, onWordClick}:Props) => {
+  const navigate = useNavigate();
+
+  const handleGoNavigation = (song : Song) => {
+    navigate(`/music/drop/${song.id}`, {state: {image:song.image}})
+  }
+
+  return (
+    <div className="MusicList">
+      {searchResults && searchResults.length > 0 ? (
+        <div className="searchResults">
+          {searchResults.map((song) => (
+            <div key={song.id} className="result-item" onClick={() => handleGoNavigation(song)}>
+              <img src={song.image}/>
+              <div className="item-wide">
+                <div className="item-detail">
+                  <div className="item-title">{song.title}</div>
+                  <div className="item-artist">{song.artist}</div>
+                </div>
+                <div className="item-length">{song.length}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <SearchedWords onWordClick={onWordClick} />
+      )}
+    </div>
+  )
+}
+
+export default MusicList
