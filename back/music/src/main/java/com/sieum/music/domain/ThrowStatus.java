9269768f9862ed9fe5862a1bace18700877d7e3b
임승연 <lsy5490@naver.com>
2024-04-23 13:51:24 +0900

    [Deploy] Merge the develop-music branch into the master branch

---
+++ back/music/src/main/java/com/sieum/music/domain/ThrowStatus.java
@@ -0,0 +1,17 @@
+package com.sieum.music.domain;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ThrowStatus {
+    VISIBLE("VISIBLE", "정상"),
+    HIDDEN("HIDDEN", "삭제"),
+    COMPLAINT("COMPLAINT", "신고");
+
+    private String value;
+
+    @JsonValue private final String kr;
+}
