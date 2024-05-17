b380337699d1de0688d3a6b7cd126081f972043e
정태윤 <wjdxodbs52@naver.com>
2024-05-17 14:44:51 +0900

    [Feature] Setting url

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,6 +4,12 @@ import { Suspense } from "react";
 import { RecoilRoot } from "recoil";
 
 function App() {
+  const url = window.location.pathname;
+
+  if (url === "/grafana-server") {
+    return null;
+  }
+
   return (
     <RecoilRoot>
       <Suspense fallback={<div></div>}>
